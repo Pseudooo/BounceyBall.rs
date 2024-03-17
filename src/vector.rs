35c50e9505fff24ece85cad9f528e4a3e471a31c
Mitchell Wright <mitchellw@digitalwonderlab.com>
2024-03-17 11:41:29 +0000

    Defined a vector struct

---
+++ src/vector.rs
@@ -0,0 +1,15 @@
+
+pub struct Vector {
+    pub i: f64,
+    pub j: f64,
+}
+
+impl Vector
+{
+    pub fn new(i: f64, j: f64) -> Vector {
+        Vector {
+            i,
+            j,
+        }
+    }
+}
