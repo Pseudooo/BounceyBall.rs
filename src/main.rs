f5eaf5f1d1d02dcfdc3e4057481e086740672920
Mitchell Wright <mitchellw@digitalwonderlab.com>
2024-03-17 13:19:31 +0000

    Scaling velocity towards mouse on click

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,17 @@ extern crate graphics;
 extern crate opengl_graphics;
 extern crate piston;
 
+use std::fmt::Debug;
+
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{GlGraphics, OpenGL};
+use piston::{Button, ButtonEvent, ButtonState, Event, Input, MouseButton, MouseCursorEvent};
 use piston::event_loop::{Events, EventSettings};
 use piston::input::{RenderEvent, UpdateEvent};
 use piston::window::WindowSettings;
 
 use crate::app::App;
+use crate::ball::Ball;
 use crate::vector::Vector;
 
 mod app;
@@ -27,7 +31,7 @@ fn main() {
         .build()
         .unwrap();
 
-    let ball = crate::ball::Ball {
+    let ball = Ball {
         x: 250.0,
         y: 250.0,
         vel: Vector::new(500.0, -25.0),
@@ -57,6 +61,30 @@ fn main() {
 
         if let Some(args) = e.update_args() {
             app.update(&args);
+        }
+
+        match e {
+            Event::Input(ref input, ..) => match input {
+                Input::Button(button) => match button.state {
+                    ButtonState::Press => match button.button {
+                        Button::Mouse(MouseButton::Left) => {
+                            let mouse_pos = &app.last_mouse_pos;
+                            let ball = &mut app.ball;
+
+                            const VEL_SCALE: f64 = 2.5;
+
+                            ball.vel.i += VEL_SCALE * (mouse_pos.i - ball.x);
+                            ball.vel.j += VEL_SCALE * (mouse_pos.j - ball.y);
+
+                            println!("CLICK");
+                        }
+                        _ => ()
+                    }
+                    _ => ()
+                }
+                _ => ()
+            }
+            _ => ()
         }
     }
 }
